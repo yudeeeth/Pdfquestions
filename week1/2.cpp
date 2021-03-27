024d70963d095043f41f114dc5bbfea243f3ed6a
yudeeeth <udithkumarvd@gmail.com>
2021-03-27 23:11:57 +0530

    --

--- week1/2.cpp
+++ week1/2.cpp
@@ -3,4 +3,14 @@
 using namespace std;
 
 int main(){
+    int k;
+    cin>>k;
+    vector<int> arr(k);
+    k=0;
+    for(auto &x : arr){
+        cin>>x;
+        k = k^x;
+    }
+    cout<<k;
+
 }
