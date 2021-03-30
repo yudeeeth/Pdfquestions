b5818123bfc1446c0ffc47ebe4934ed3eac50b4b
yudeeeth <udithkumarvd@gmail.com>
2021-03-30 22:47:41 +0530

    --

--- week1/b4.cpp
+++ week1/b4.cpp
@@ -1,15 +1,30 @@
+    //dutch flag and naive solution;
     void sort012(int a[], int n)
     {
         // coode here 
-        int c[3];
-        c[0]=0;c[1]=0;c[2]=0;
-        for(int i=0;i<n;i++){
-            c[a[i]]++;
+        // int c[3];
+        // c[0]=0;c[1]=0;c[2]=0;
+        // for(int i=0;i<n;i++){
+        //     c[a[i]]++;
+        // }
+        // int i;
+        // for(i=0;i<c[0];i++){
+        //     a[i]=0;
+        // }
+        // for(;i<c[0]+c[1];i++) a[i]=1;
+        // for(;i<c[0]+c[1]+c[2];i++) a[i]=2;
+        int l,m,h;
+        l=0;m=0;h=n-1;
+        int flag=0;
+        while(m<=h || flag==0){
+            if(m==h) flag=1;
+            if(a[m]==0) {swap(a,l,m);l++;m++;}
+            else if(a[m]==2) {swap(a,h,m);h--;}
+            else m++;
         }
-        int i;
-        for(i=0;i<c[0];i++){
-            a[i]=0;
-        }
-        for(;i<c[0]+c[1];i++) a[i]=1;
-        for(;i<c[0]+c[1]+c[2];i++) a[i]=2;
+    }
+    void swap(int arr[],int i,int j){
+        int temp =arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     }
