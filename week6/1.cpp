9039b6b00aa6773f41b27298a888611be6a2f0a7
yudeeeth <udithkumarvd@gmail.com>
2021-05-27 11:41:40 +0530

    --

---
+++ week6/1.cpp
@@ -0,0 +1,15 @@
+/* Tree node structure  used in the program
+ struct Node
+ {
+     int data;
+     Node* left;
+     Node* right;
+}; */
+
+/* Computes the number of nodes in a tree. */
+int getSize(Node* node)
+{
+    if(node==NULL) return 0;
+    return 1 + getSize(node->left)+getSize(node->right);
+   // Your code here
+}
